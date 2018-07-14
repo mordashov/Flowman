0f07094ed3198cb692255083dd92739aa0217b03
mordashov <mordashovv@gmail.com>
2018-07-14 17:04:14 +0300

    Заработал DataGrid, список менеджеров, выбор года

--- Flow management/MainWindow.xaml.cs
+++ Flow management/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,57 @@ namespace Flow_management
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Обновление DataGrid
+        private void GenerateDataGridReq(string sql)
+        {
+            MsAccess acs = new MsAccess();
+            DataGridRequests.DataContext = acs.CreateDataTable(sql);
+        }
+
+        //Генерация списка менеджеров для комбобокс
+        private void GenerateComboBoxMp(string sql)
+        {
+            MsAccess acs = new MsAccess();
+            ComboBoxMp.ItemsSource = acs.CreateDataTable(sql).DefaultView;
+        }
+
+        //Генерация списка годов для комбобокс присутствующих в потоке заявок
+        private void GenerateComboBoxYear(string sql)
+        {
+            MsAccess acs = new MsAccess();
+            ComboBoxYear.ItemsSource = acs.CreateDataTable(sql).DefaultView;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string sql = @"
+                            SELECT 
+                            mng.mng_fln AS МП
+                            , ord.ord_num AS Обращение
+                            , ord.ord_dt AS Дата
+                            , stf.stf_fln AS Сотрудник
+                            , pos.pos_nm AS Должность
+                            , dep.dep_mn AS Отдел
+                            , """" AS Норма
+                            , """" AS Итого
+                            , """" AS [Норма отдела]
+                            , """" AS [Итого по отделу]
+                            FROM (dep INNER JOIN stf ON dep.dep_id = stf.dep_id) 
+                                INNER JOIN (pos INNER JOIN (ord INNER JOIN (mng INNER JOIN flw ON mng.mng_th = flw.mng_tn) 
+                                ON ord.ord_id = flw.ord_id) 
+                                ON pos.pos_id = flw.pos_id) ON stf.stf_tn = flw.stf_tn;
+                            ";
+            GenerateDataGridReq(sql);
+
+            sql = "SELECT mng.mng_th as id, mng.mng_fln as Name FROM mng; ";
+            GenerateComboBoxMp(sql);
+
+            sql = @"SELECT Year([ord_dt]) AS Год
+                    FROM ord
+                    GROUP BY Year([ord_dt]);";
+            GenerateComboBoxYear(sql);
         }
     }
 }
---
+++ Flow management/MsAccess.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Flow_management
+{
+
+    class MsAccess
+    {
+        private string _pathToBase = null;
+        private string _connectionString = null;
+
+
+        public string PathToBase
+        {
+            get => _pathToBase;
+            set => _pathToBase = value;
+        }
+
+        public string ConnectionString
+        {
+            get => _connectionString;
+            set => _connectionString = value;
+        }
+
+        //Проверка наличия файла
+        private void CheckPathToBase(string path)
+        {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("Файл " + path + " не найден!\nПрограмма будет закрыта");
+                Environment.Exit(0);
+            }
+        }
+
+        //Получение соединения с базой данных
+        public OleDbConnection CreateConnection()
+        {
+            //Проверка наличия файла настроек
+            string currentPath = Environment.CurrentDirectory + "\\settings.txt";
+            CheckPathToBase(currentPath);
+
+            //Проверка наличия файла базы данных
+            var line = File.ReadLines(currentPath).Take(1);
+            _pathToBase = line.First();
+            CheckPathToBase(_pathToBase);
+
+            //Подключение к базе данных
+            OleDbConnection connection = new OleDbConnection();
+            _connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + _pathToBase;
+
+            //_connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Dropbox\SrOrder\ord.accdb";
+
+            connection.ConnectionString = _connectionString;
+
+            //Проверяю подключение
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Не могу подключиться к базе данных " + _pathToBase + "\n\n" + ex.ToString());
+                Environment.Exit(0);
+                return null;
+            }
+        }
+
+        //Получаю DataTable
+        public DataTable CreateDataTable(string sql)
+        {
+
+            DataTable dataTable = new DataTable();
+
+            OleDbConnection connection = CreateConnection();
+            OleDbCommand command = new OleDbCommand(sql, connection);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            return dataTable;
+        }
+    }
+}
