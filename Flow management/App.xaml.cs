ace9136f7dc196436605fad3d080a2aba54cd829
mordashov <mordashovv@gmail.com>
2018-08-08 21:58:22 +0300

    Добавил возможность запустить окно нормы через аргумент приложения

--- Flow management/App.xaml.cs
+++ Flow management/App.xaml.cs
@@ -12,5 +12,29 @@ namespace Flow_management
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            bool showNorm = false;
+            for (int i = 0; i != e.Args.Length; ++i)
+            {
+                if (e.Args[i] == "/norms")
+                {
+                    showNorm = true;
+                }
+            }
+
+           
+            if (showNorm)
+            {
+                Norm normWindow = new Norm();
+                normWindow.Show();
+            }
+            else
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            
+        }
     }
 }
